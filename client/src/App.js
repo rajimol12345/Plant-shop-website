a016f8c8ceb8091a7a86394a4be46427c25eeaa2
rajimol12345 <rajimol3092002@gmail.com>
2026-04-01 15:11:34 +0530

    fixed build stuck issue: optimized memory, added CI=false and GENERATE_SOURCEMAP=false, implemented lazy loading in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,40 +6,42 @@ import { useDispatch, useSelector } from 'react-redux';
 import Header from './components/Header';
 import ChatWidget from './components/ChatWidget';
 import AdminChatWidget from './components/AdminChatWidget';
-import HomeScreen from './screens/HomeScreen';
+import Loader from './components/Loader';
 import { getCart } from './actions/cartActions';
 
 import Footer from './components/Footer';
 import InstagramFeed from './components/InstagramFeed';
 
-import ShopScreen from './screens/ShopScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ShippingScreen from './screens/ShippingScreen';
-import PaymentScreen from './screens/PaymentScreen';
-import PlaceOrderScreen from './screens/PlaceOrderScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import UserListScreen from './screens/UserListScreen';
-import ProductListScreen from './screens/ProductListScreen';
-import ProductEditScreen from './screens/ProductEditScreen';
-import OrderScreen from './screens/OrderScreen';
-import OrderListScreen from './screens/OrderListScreen';
-import OrderSuccessScreen from './screens/OrderSuccessScreen';
-import AdminRoute from './components/AdminRoute';
-import AboutScreen from './screens/AboutScreen';
-import TeamScreen from './screens/TeamScreen';
-import FAQScreen from './screens/FAQScreen';
-import BlogScreen from './screens/BlogScreen';
-import BlogDetailScreen from './screens/BlogDetailScreen';
-import ContactScreen from './screens/ContactScreen';
-import DashboardScreen from './screens/DashboardScreen';
-import BlogListScreen from './screens/BlogListScreen';
-import BlogEditScreen from './screens/BlogEditScreen';
-import NewsletterListScreen from './screens/NewsletterListScreen';
-import AdminChatScreen from './screens/AdminChatScreen';
-import NotFoundScreen from './screens/NotFoundScreen';
+// Lazy loading screens
+const HomeScreen = lazy(() => import('./screens/HomeScreen'));
+const ShopScreen = lazy(() => import('./screens/ShopScreen'));
+const ProductScreen = lazy(() => import('./screens/ProductScreen'));
+const CartScreen = lazy(() => import('./screens/CartScreen'));
+const LoginScreen = lazy(() => import('./screens/LoginScreen'));
+const RegisterScreen = lazy(() => import('./screens/RegisterScreen'));
+const ShippingScreen = lazy(() => import('./screens/ShippingScreen'));
+const PaymentScreen = lazy(() => import('./screens/PaymentScreen'));
+const PlaceOrderScreen = lazy(() => import('./screens/PlaceOrderScreen'));
+const ProfileScreen = lazy(() => import('./screens/ProfileScreen'));
+const UserListScreen = lazy(() => import('./screens/UserListScreen'));
+const ProductListScreen = lazy(() => import('./screens/ProductListScreen'));
+const ProductEditScreen = lazy(() => import('./screens/ProductEditScreen'));
+const OrderScreen = lazy(() => import('./screens/OrderScreen'));
+const OrderListScreen = lazy(() => import('./screens/OrderListScreen'));
+const OrderSuccessScreen = lazy(() => import('./screens/OrderSuccessScreen'));
+const AdminRoute = lazy(() => import('./components/AdminRoute'));
+const AboutScreen = lazy(() => import('./screens/AboutScreen'));
+const TeamScreen = lazy(() => import('./screens/TeamScreen'));
+const FAQScreen = lazy(() => import('./screens/FAQScreen'));
+const BlogScreen = lazy(() => import('./screens/BlogScreen'));
+const BlogDetailScreen = lazy(() => import('./screens/BlogDetailScreen'));
+const ContactScreen = lazy(() => import('./screens/ContactScreen'));
+const DashboardScreen = lazy(() => import('./screens/DashboardScreen'));
+const BlogListScreen = lazy(() => import('./screens/BlogListScreen'));
+const BlogEditScreen = lazy(() => import('./screens/BlogEditScreen'));
+const NewsletterListScreen = lazy(() => import('./screens/NewsletterListScreen'));
+const AdminChatScreen = lazy(() => import('./screens/AdminChatScreen'));
+const NotFoundScreen = lazy(() => import('./screens/NotFoundScreen'));
 
 function App() {
   const dispatch = useDispatch();
@@ -57,100 +59,102 @@ function App() {
       <ToastContainer position="top-right" autoClose={3000} />
       <Header />
       <main>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/about" element={<AboutScreen />} />
-          <Route path="/team" element={<TeamScreen />} />
-          <Route path="/faq" element={<FAQScreen />} />
-          <Route path="/blog" element={<BlogScreen />} />
-          <Route path="/blog/:id" element={<BlogDetailScreen />} />
-          <Route path="/contact" element={<ContactScreen />} />
-          <Route path="/shop" element={<ShopScreen />} />
-          <Route path="/shop/search/:keyword" element={<ShopScreen />} />
-          <Route path="/product/:id" element={<ProductScreen />} />
-          <Route path="/cart/:id?" element={<CartScreen />} />
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/register" element={<RegisterScreen />} />
-          <Route path="/shipping" element={<ShippingScreen />} />
-          <Route path="/payment" element={<PaymentScreen />} />
-          <Route path="/placeorder" element={<PlaceOrderScreen />} />
-          <Route path="/ordersuccess" element={<OrderSuccessScreen />} />
-          <Route path="/profile" element={<ProfileScreen />} />
-          <Route path="/order/:id" element={<OrderScreen />} />
-          <Route
-            path="/admin/userlist"
-            element={
-              <AdminRoute>
-                <UserListScreen />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/productlist"
-            element={
-              <AdminRoute>
-                <ProductListScreen />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/dashboard"
-            element={
-              <AdminRoute>
-                <DashboardScreen />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/product/:id/edit"
-            element={
-              <AdminRoute>
-                <ProductEditScreen />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/orderlist"
-            element={
-              <AdminRoute>
-                <OrderListScreen />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/bloglist"
-            element={
-              <AdminRoute>
-                <BlogListScreen />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/blog/:id/edit"
-            element={
-              <AdminRoute>
-                <BlogEditScreen />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/newsletterlist"
-            element={
-              <AdminRoute>
-                <NewsletterListScreen />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/chat"
-            element={
-              <AdminRoute>
-                <AdminChatScreen />
-              </AdminRoute>
-            }
-          />
-          <Route path="*" element={<NotFoundScreen />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/about" element={<AboutScreen />} />
+            <Route path="/team" element={<TeamScreen />} />
+            <Route path="/faq" element={<FAQScreen />} />
+            <Route path="/blog" element={<BlogScreen />} />
+            <Route path="/blog/:id" element={<BlogDetailScreen />} />
+            <Route path="/contact" element={<ContactScreen />} />
+            <Route path="/shop" element={<ShopScreen />} />
+            <Route path="/shop/search/:keyword" element={<ShopScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart/:id?" element={<CartScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/shipping" element={<ShippingScreen />} />
+            <Route path="/payment" element={<PaymentScreen />} />
+            <Route path="/placeorder" element={<PlaceOrderScreen />} />
+            <Route path="/ordersuccess" element={<OrderSuccessScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
+            <Route path="/order/:id" element={<OrderScreen />} />
+            <Route
+              path="/admin/userlist"
+              element={
+                <AdminRoute>
+                  <UserListScreen />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin/productlist"
+              element={
+                <AdminRoute>
+                  <ProductListScreen />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin/dashboard"
+              element={
+                <AdminRoute>
+                  <DashboardScreen />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin/product/:id/edit"
+              element={
+                <AdminRoute>
+                  <ProductEditScreen />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin/orderlist"
+              element={
+                <AdminRoute>
+                  <OrderListScreen />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin/bloglist"
+              element={
+                <AdminRoute>
+                  <BlogListScreen />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin/blog/:id/edit"
+              element={
+                <AdminRoute>
+                  <BlogEditScreen />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin/newsletterlist"
+              element={
+                <AdminRoute>
+                  <NewsletterListScreen />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin/chat"
+              element={
+                <AdminRoute>
+                  <AdminChatScreen />
+                </AdminRoute>
+              }
+            />
+            <Route path="*" element={<NotFoundScreen />} />
+          </Routes>
+        </Suspense>
       </main>
       <InstagramFeed />
       <Footer />
